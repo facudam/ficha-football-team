79db206c8e9967df4528cc21036196af9b2f8ff2
facudam <facundo89caceres@gmail.com>
2022-05-18 16:34:34 -0300

    insertamos los jugadores en etiquetas li definidas en el html con la api

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,19 @@ const $seccionDeEquipos = document.querySelector('.secciones__equipos'),
     $boton = document.querySelector('.btn'),
     $fragmento = document.createDocumentFragment();
 
+//Jugadores li:
+ const $jugador1 = document.querySelector('.jugador1'),
+    $jugador2 = document.querySelector('.jugador2'),
+    $jugador3 = document.querySelector('.jugador3'),
+    $jugador4 = document.querySelector('.jugador4'),
+    $jugador5 = document.querySelector('.jugador5'),
+    $jugador6 = document.querySelector('.jugador6'),
+    $jugador7 = document.querySelector('.jugador7'),
+    $jugador8 = document.querySelector('.jugador8'),
+    $jugador9 = document.querySelector('.jugador9'),
+    $jugador10 = document.querySelector('.jugador10'),
+    $jugador11= document.querySelector('.jugador11');
+
 
 const apiKey = '971aa7c8535609f353f13813698ac2a80be95ad948ce74fcf45137acc415e49b';
 
@@ -15,20 +28,24 @@ let num = 0;
 function verEquipos () {
 
     fetch(`https://apiv2.allsportsapi.com/football/?&met=Teams&teamId=${equiposNumeros[num]}&APIkey=${apiKey}`)
-        .then(res => res.json()) //transformamos la respuesta de la api a formato json.
+        .then(res => res.json()) 
         .then(data => {
             console.log(data)
             $nombreEquipo.innerHTML = `${data.result[0].team_name}`;
             $logoEquipo.setAttribute('src', `${data.result[0].team_logo}`);
             $dt.innerHTML = `Director Técnico: ${data.result[0].coaches[0].coach_name}`;
 
-            for ( let i = 0; i < 11; i++){
-                const $li = document.createElement('li');
-                $li.innerHTML += `${data.result[0].players[i].player_name}`;
-                $fragmento.appendChild($li);
-            }
-            $jugadores.appendChild($fragmento);
-            
+            $jugador1.innerHTML = `${data.result[0].players[0].player_name}`;
+            $jugador2.innerHTML = `${data.result[0].players[1].player_name}`;
+            $jugador3.innerHTML = `${data.result[0].players[2].player_name}`;
+            $jugador4.innerHTML = `${data.result[0].players[3].player_name}`;
+            $jugador5.innerHTML = `${data.result[0].players[4].player_name}`;
+            $jugador6.innerHTML = `${data.result[0].players[5].player_name}`;
+            $jugador7.innerHTML = `${data.result[0].players[6].player_name}`;
+            $jugador8.innerHTML = `${data.result[0].players[7].player_name}`;
+            $jugador9.innerHTML = `${data.result[0].players[8].player_name}`;
+            $jugador10.innerHTML = `${data.result[0].players[9].player_name}`;
+            $jugador11.innerHTML = `${data.result[0].players[10].player_name}`; 
             
         }) 
 }
